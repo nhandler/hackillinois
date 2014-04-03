782ad344a9b8158c9a49ba3439e4974bc875ff8a
Nathan Handler <nhandler@debian.org>
2014-04-03 12:12:50 -0500

    Add second time

--- src/pebble-hackillinois.c
+++ src/pebble-hackillinois.c
@@ -2,14 +2,18 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static TextLayer *text_layer2;
 static char buffer[] = "00:00";
+static char buffer2[] = "00:00";
 
 static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
   // Format the buffer string using tick_time as the time source
   strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+  strftime(buffer2, sizeof("00:00"), "%H:%M", tick_time);
 
   // Change the TextLayer text to show the new time
   text_layer_set_text(text_layer, buffer);
+  text_layer_set_text(text_layer2, buffer2);
 }
 
 static void window_load(Window *window) {
@@ -23,6 +27,13 @@ static void window_load(Window *window) {
   text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
+  text_layer2 = text_layer_create((GRect) { .origin = { 0, 75 }, .size = { bounds.size.w, 70 } });
+  text_layer_set_text_alignment(text_layer2, GTextAlignmentCenter);
+  text_layer_set_background_color(text_layer2, GColorBlack);
+  text_layer_set_text_color(text_layer2, GColorWhite);
+  text_layer_set_font(text_layer2, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  layer_add_child(window_layer, text_layer_get_layer(text_layer2));
+
   // Get a time structure so that the face doesn't start blank
   struct tm *t;
   time_t temp;
@@ -35,6 +46,7 @@ static void window_load(Window *window) {
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
+  text_layer_destroy(text_layer2);
 }
 
 static void init(void) {
