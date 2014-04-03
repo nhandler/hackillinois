b7802dba5df4c820b5914308938e1affa32c63d1
Nathan Handler <nhandler@debian.org>
2014-04-03 11:57:44 -0500

    Initial watchface commit

--- src/pebble-hackillinois.c
+++ src/pebble-hackillinois.c
@@ -2,37 +2,39 @@
 
 static Window *window;
 static TextLayer *text_layer;
-
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
-}
-
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
+static char buffer[] = "00:00";
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 70 } });
   text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  text_layer_set_background_color(text_layer, GColorBlack);
+  text_layer_set_text_color(text_layer, GColorWhite);
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
+
+  // Get a time structure so that the face doesn't start blank
+  struct tm *t;
+  time_t temp;
+  temp = time(NULL);
+  t = localtime(&temp);
+
+  // Manually call the tick handler when the window is loading
+  tick_handler(t, MINUTE_UNIT);
 }
 
 static void window_unload(Window *window) {
   text_layer_destroy(text_layer);
+}
+
+static void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
+  // Format the buffer string using tick_time as the time source
+  strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+
+  // Change the TextLayer text to show the new time
+  text_layer_set_text(text_layer, buffer);
 }
 
 static void init(void) {
@@ -43,18 +45,19 @@ static void init(void) {
     .unload = window_unload,
   });
   const bool animated = true;
+  window_set_background_color(window, GColorBlack);
   window_stack_push(window, animated);
+
+  tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 }
 
 static void deinit(void) {
+  tick_timer_service_unsubscribe();
   window_destroy(window);
 }
 
 int main(void) {
   init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
   app_event_loop();
   deinit();
 }
